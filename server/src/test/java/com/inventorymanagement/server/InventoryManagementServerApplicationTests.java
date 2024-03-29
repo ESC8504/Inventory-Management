14dc11bae30f75c396bf6f4ed29459555c8e1367
Eric Chang <fsc8504@gmail.com>
2024-03-28 18:06:03 -0700

    Create basic project strcture

---
+++ server/src/test/java/com/inventorymanagement/server/InventoryManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventorymanagement.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
